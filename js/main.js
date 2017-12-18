5a3777e463c265daa6950acc999579183069d100
janakitti <janakittiratana@gmail.com>
2017-12-18 15:50:18 -0500

    Added findDirection function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// JavaScript Document // Note: This example requires that you consent to location sharing when
+// Note: This example requires that you consent to location sharing when
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
@@ -18,8 +18,8 @@
               lng: position.coords.longitude
             };
             var output = pos.lat + "°N, " + pos.lng + "°E";
-			document.getElementById("userLocation").innerHTML = output;
-			calcRisk(pos.lng, pos.lat);
+      document.getElementById("userLocation").innerHTML = output;
+      calcRisk(pos.lng, pos.lat);
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
@@ -33,12 +33,12 @@
           handleLocationError(false, infoWindow, map.getCenter());
         }
       }
-	function calcRisk(curLocLong, curLocLat) {
-		// Set coords
+  function calcRisk(curLocLong, curLocLat) {
+    // Set coords
     var coords = [];
     var squareRange = [];
     var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           var jsonData = JSON.parse(this.responseText);
           for (i = 0; i < jsonData.features.length; i++) {
@@ -46,56 +46,98 @@
           }
 
           // console.log(coords);
-          longCov = 80.00;
-      		latCov = 111.045;
-      		// Set current location
-      		var curLoc = [curLocLong, curLocLat];
-      		// Set radius, in kilometers
-      		var r = 1;
-      		document.getElementById("radius").innerHTML = r;
-			document.getElementById("radius2").innerHTML = r;
+          var longCov = 80.00;
+          var latCov = 111.045;
+          // Set current location
+          var curLoc = [curLocLong, curLocLat];
+          // Set radius, in kilometers
+          var r = 1;
+          document.getElementById("radius").innerHTML = r;
+      document.getElementById("radius2").innerHTML = r;
 
-      		// Initialize square range
-      		// Scan thru and save all  points withing square range
+          // Initialize square range
+          // Scan thru and save all  points withing square range
           // console.log(coords);
           // console.log(curLoc);
 
-      		for (var i = 0; i < coords.length; i++) {
-      			if (coords[i].x < curLoc[0] + (r / longCov)
-      			  && coords[i].x > curLoc[0] - (r / longCov)
-      			  && coords[i].y < curLoc[1] + (r / latCov)
-      			  && coords[i].y > curLoc[1] - (r / latCov))
+          for (var i = 0; i < coords.length; i++) {
+            if (coords[i].x < curLoc[0] + (r / longCov)
+              && coords[i].x > curLoc[0] - (r / longCov)
+              && coords[i].y < curLoc[1] + (r / latCov)
+              && coords[i].y > curLoc[1] - (r / latCov))
               {
-      				squareRange.push([coords[i].x, coords[i].y]);
-      			}
-      		}
+              squareRange.push([coords[i].x, coords[i].y]);
+            }
+          }
           console.log(squareRange);
-		  document.getElementById("crimeCount").innerHTML = squareRange.length;
+      document.getElementById("crimeCount").innerHTML = squareRange.length;
 
-      		//square root value
-      		var countSquareRange = Math.floor(Math.sqrt(squareRange.length) * 10) /10;
+          //square root value
+          var countSquareRange = Math.floor(Math.sqrt(squareRange.length) * 10) /10;
 
 
 
-      		if (Math.floor(Math.sqrt(squareRange.length) * 10) /10 > 10) {
-      			countSquareRange = 10;
+          if (Math.floor(Math.sqrt(squareRange.length) * 10) /10 > 10) {
+            countSquareRange = 10;
 
-      		}
-      		// output to webpage
-      		document.getElementById("crimeRatingText").innerHTML = countSquareRange + "/10";
-			
-			document.getElementById("scale").style.width = countSquareRange * 10 + "%";
+          }
+          // output to webpage
+          document.getElementById("crimeRatingText").innerHTML = countSquareRange + "/10";
 
+      document.getElementById("scale").style.width = countSquareRange * 10 + "%";
       }
 
+      findDirection(squareRange, curLoc, latCov, longCov, r);
+
     };
-
     xmlhttp.open("GET", "https://burntpotatoes.github.io/safety-way/Crime%20Data/CrimeData.json", true);
-
     xmlhttp.send();
-		//pos.long and pos.lat
+    //pos.long and pos.lat
+  }
 
-	}
+function findDirection(squareRange, curLoc, latCov, longCov, r) {
+    var northCoords = [];
+    var southCoords = [];
+    var eastCoords = [];
+    var westCoords = [];
+    var directionList = [];
+
+    for (var i = 0; i < squareRange.length; i++) {
+      if (squareRange[i][1] < curLoc[0] + (r / latCov) && squareRange[i][0] > curLoc[0]) {
+        northCoords.push([squareRange[i].x, squareRange[i].y]);
+      }
+      else if (squareRange[i][1] > curLoc[0] - (r / latCov) && squareRange[i][0] < curLoc[0]) {
+        southCoords.push([squareRange[i].x, squareRange[i].y]);
+      }
+      else if (squareRange[i][0] < curLoc[0] + (r / longCov) && squareRange[i][1] > curLoc[0]) {
+        eastCoords.push([squareRange[i].x, squareRange[i].y]);
+      }
+      else if (squareRange[i][0]> curLoc[0] - (r / longCov) && squareRange[i][1] < curLoc[0]) {
+        westCoords.push([squareRange[i].x, squareRange[i].y]);
+      }
+    }
+    var northCount = northCoords.length;
+    var southCount = southCoords.length;
+    var eastCount = eastCoords.length;
+    var westCount = westCoords.length;
+
+    directionList.push(northCount, southCount, eastCount, westCount);
+    console.log(directionList);
+
+    var indexOfSafestDirection = directionList.indexOf(Math.min(directionList));
+    if (indexOfSafestDirection = 0) {
+      console.log("NORTH");
+    }
+    else if (indexOfSafestDirection = 1) {
+      console.log("SOUTH");
+    }
+    else if (indexOfSafestDirection = 2) {
+      console.log("EAST");
+    }
+    else if (indexOfSafestDirection = 3) {
+      console.log("WEST");
+    }
+}
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
