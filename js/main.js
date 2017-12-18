4c0b425398585d36061667055c69feb3160ab7ba
Ningsong Shen <heliotrope3n@users.noreply.github.com>
2017-12-18 15:46:10 -0500

    clean up the (messy) code

---
+++ js/main.js
@@ -0,0 +1,106 @@
+// JavaScript Document // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var output = pos.lat + "°N, " + pos.lng + "°E";
+			document.getElementById("userLocation").innerHTML = output;
+			calcRisk(pos.lng, pos.lat);
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+	function calcRisk(curLocLong, curLocLat) {
+		// Set coords
+    var coords = [];
+    var squareRange = [];
+    var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var jsonData = JSON.parse(this.responseText);
+          for (i = 0; i < jsonData.features.length; i++) {
+            coords.push({'x':jsonData.features[i].geometry.x, 'y':jsonData.features[i].geometry.y});
+          }
+
+          // console.log(coords);
+          longCov = 80.00;
+      		latCov = 111.045;
+      		// Set current location
+      		var curLoc = [curLocLong, curLocLat];
+      		// Set radius, in kilometers
+      		var r = 1;
+      		document.getElementById("radius").innerHTML = r;
+			document.getElementById("radius2").innerHTML = r;
+
+      		// Initialize square range
+      		// Scan thru and save all  points withing square range
+          // console.log(coords);
+          // console.log(curLoc);
+
+      		for (var i = 0; i < coords.length; i++) {
+      			if (coords[i].x < curLoc[0] + (r / longCov)
+      			  && coords[i].x > curLoc[0] - (r / longCov)
+      			  && coords[i].y < curLoc[1] + (r / latCov)
+      			  && coords[i].y > curLoc[1] - (r / latCov))
+              {
+      				squareRange.push([coords[i].x, coords[i].y]);
+      			}
+      		}
+          console.log(squareRange);
+		  document.getElementById("crimeCount").innerHTML = squareRange.length;
+
+      		//square root value
+      		var countSquareRange = Math.floor(Math.sqrt(squareRange.length) * 10) /10;
+
+
+
+      		if (Math.floor(Math.sqrt(squareRange.length) * 10) /10 > 10) {
+      			countSquareRange = 10;
+
+      		}
+      		// output to webpage
+      		document.getElementById("crimeRatingText").innerHTML = countSquareRange + "/10";
+			
+			document.getElementById("scale").style.width = countSquareRange * 10 + "%";
+
+      }
+
+    };
+
+    xmlhttp.open("GET", "https://burntpotatoes.github.io/safety-way/Crime%20Data/CrimeData.json", true);
+
+    xmlhttp.send();
+		//pos.long and pos.lat
+
+	}
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
